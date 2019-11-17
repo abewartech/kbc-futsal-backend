ecabdd2ab9043912a6f2813010b48fd1785e325a
Your Name <you@example.com>
2019-11-17 10:28:45 +0700

    AB : use gzip compress

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const compression = require("compression");
 const server = require("http").createServer(app);
 const middleware = require("./config/middleware");
 const Routes = require("./routes/index.js");
 require("dotenv").config();
 
 middleware(app);
+app.use(compression());
 app.use(express.static("public"));
 app.use("/api", Routes);
 
